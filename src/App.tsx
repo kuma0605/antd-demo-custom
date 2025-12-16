825395010c2d7891f7f27741eb1b55ac89000689
dylan <825880445@qq.com>
2025-12-16 23:03:42 +0800

    安装tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -13,33 +13,46 @@ import zhCN from 'antd/locale/zh_CN';
 
 dayjs.locale('zh-cn'); // 设置dayjs为中文
 
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
+
 function App() {
   
   const [count, setCount] = useState(0)
 
-  return (
-    <ConfigProvider locale={zhCN}>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+  return (// Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider locale={zhCN}>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </ConfigProvider>
+      </ConfigProvider>
+    </QueryClientProvider>
   )
 }
 
