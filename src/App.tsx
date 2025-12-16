95509febbae24d19e2e33f09dea2d19df3e5a03d
dylan <825880445@qq.com>
2025-12-16 19:55:45 +0800

    antd种文化

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { ConfigProvider, DatePicker, type DatePickerProps } from 'antd';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import dayjs from 'dayjs';
+
+import 'dayjs/locale/zh-cn';
+
+import zhCN from 'antd/locale/zh_CN';
+
+dayjs.locale('zh-cn'); // 设置dayjs为中文
+
 function App() {
+  const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+    console.log(date, dateString);
+  };
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ConfigProvider locale={zhCN}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,6 +30,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <DatePicker onChange={onChange} />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,7 +42,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ConfigProvider>
   )
 }
 
